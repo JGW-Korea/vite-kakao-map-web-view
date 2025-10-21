0f1b543504957b317c63ec55422a3fc67651b78b
JGW-Korea <jgw6372@gmail.com>
2025-10-21 16:53:18 +0900

    update: 카카오 지도 전역 상태 구성 수정

--- src/components/KakaoMap/kakaoMap.store.ts
+++ src/components/KakaoMap/kakaoMap.store.ts
@@ -3,15 +3,30 @@ import { MapInstance } from "../../types/kakaoMapInstance.type";
 
 interface Map {
   kakaoMap: MapInstance | null;
-  kakaoMapInit: (state: MapInstance) => void;
+  granted: boolean;
+  location: {
+    latitude: number;
+    longitude: number;
+  }
+  kakaoMapInit: (state: MapInstance, granted: boolean, location: { latitude: number; longitude: number }) => void;
 }
 
 // 생성한 카카오 지도를 전역에서 공유하기 위한 Zustand 전역 스토어
 export const useMapStore = create<Map>((set) => ({
   kakaoMap: null,
-  kakaoMapInit: (state: MapInstance) => {
+  granted: false,
+  location: {
+    latitude: 0,
+    longitude: 0,
+  },
+  kakaoMapInit: (map: MapInstance, granted: boolean, location) => {
     set({
-      kakaoMap: state,
+      kakaoMap: map,
+      granted,
+      location: {
+        latitude: location.latitude,
+        longitude: location.longitude
+      }
     });
   },
 }));
